396b4d938655d9c5af4e0c6055e50739c981dd38
KwangHun-Song <overgive@naver.com>
2023-09-23 18:48:43 +0900

    플레이어탭 추가

--- Editor/TestRecordWriterEditor.cs
+++ Editor/TestRecordWriterEditor.cs
@@ -3,26 +3,84 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
+using Cysharp.Threading.Tasks;
 using Newtonsoft.Json;
 using UnityEditor;
 using UnityEngine;
 
 namespace P1SModule.TestRecorder.Editor {
     public class TestRecordWriterEditor : EditorWindow {
+        private enum Tab { Writer, Player }
+        
         private RaycastDetector raycastDetector;
         private TestRecordWriter writer;
+        private TestRecordPlayer player;
         private List<(PreDesignedFuncAttribute funcAttr, MethodInfo method)> preDesignedFuncs;
         
         private string savePath = "Assets/Resources/TestRecord";
         private string testName;
+        private Tab currentTab = Tab.Writer;
 
         private void OnGUI() {
             if (Application.isPlaying == false) {
                 EditorGUILayout.LabelField("게임을 실행해야 UI가 나타납니다.");
                 return;
             }
-
+            
             savePath = EditorGUILayout.TextField("파일 저장 위치", savePath);
+            currentTab = (Tab)GUILayout.Toolbar((int)currentTab, Enum.GetNames(typeof(Tab)));
+
+            switch (currentTab) {
+                case Tab.Writer:
+                    DrawWriterTab();
+                    break;
+                case Tab.Player:
+                    DrawPlayerTab();
+                    break;
+            }
+        }
+
+        private void DrawPlayerTab() {
+            // savePath에서 "*.json" 파일 검색
+            var jsonFiles = Directory.GetFiles(savePath, "*.json");
+            if (jsonFiles.Length == 0) {
+                EditorGUILayout.LabelField("No json files found.");
+                return;
+            }
+
+            if (player is { IsPlaying: true }) {
+                EditorGUILayout.LabelField("플레이어 실행중");
+            }
+
+            // 파일 이름들을 읽어와서 foreach 돌면서 GUILayout.Button으로 분기
+            foreach (var filePath in jsonFiles) {
+                var fileName = Path.GetFileNameWithoutExtension(filePath);
+                if (GUILayout.Button(fileName)) {
+                    player ??= new TestRecordPlayer(new SimpleLogPlayerEvent());
+                    player.Start(JsonConvert.DeserializeObject<TestRecord>(File.ReadAllText($"{savePath}/{fileName}.json")));
+                    WaitAndSaveReportAsync().Forget();
+                }
+            }
+        }
+
+        private async UniTask WaitAndSaveReportAsync() {
+            var result = await player.WaitUntilTestEnd();
+            var sb = new StringBuilder();
+            sb.AppendLine($"테스트 이름 : {result.testName}");
+            sb.AppendLine($"테스트 결과 : {result.result}");
+            sb.AppendLine($"테스트 진행과정 :");
+            foreach (var stepReport in result.stepReports) {
+                sb.AppendLine($"\t{stepReport.result} >> {stepReport.comment}");
+            }
+            
+            File.WriteAllText($"{savePath}/{result.testName}_result.txt", sb.ToString());
+            player = null;
+
+            EditorUtility.DisplayDialog("완료", $"플레이가 완료되었습니다. 결과는 {savePath}/{result.testName}_result.txt 에 저장되었습니다.", "OK");
+        }
+
+        private void DrawWriterTab() {
             testName = EditorGUILayout.TextField("테스트 이름", testName);
             
             EditorGUILayout.Space();
@@ -50,6 +108,8 @@ namespace P1SModule.TestRecorder.Editor {
             
             DrawOnRecording();
         }
+
+        #region WriterTab
 
         private void DrawNotRecording() {
             if (GUILayout.Button("기록 시작")) {
@@ -99,6 +159,8 @@ namespace P1SModule.TestRecorder.Editor {
                 .Where(pair => pair.Attribute != null)
                 .ToList();
         }
+
+        #endregion
 
         #region EditowWindow
 
